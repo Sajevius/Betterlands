700dcd0df4a156c661af50296caacfd60e3374f0
Sajevius Crend <71295972+Sajevius@users.noreply.github.com>
2021-11-03 16:54:25 +0800

    Update BetterlandsVanillaCompat.java

--- src/main/java/com/sajevius/betterlands/util/BetterlandsVanillaCompat.java
+++ src/main/java/com/sajevius/betterlands/util/BetterlandsVanillaCompat.java
@@ -1,4 +1,6 @@
 package com.sajevius.betterlands.util;
 
 public class BetterlandsVanillaCompat {
+    // heavy work in progress
+    // expect stuff like custom log stripping & custom compostables registry here
 }
